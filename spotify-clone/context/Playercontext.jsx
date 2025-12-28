c6b85b5f944ca0a490a7c8aa64b8233e5ec2a162
Venkatesan <vp2064670@gamil.com>
2025-12-28 17:16:05 +0530

    initial commit

---
+++ spotify-clone/context/Playercontext.jsx
@@ -0,0 +1,103 @@
+import { createContext, useEffect, useRef, useState, } from "react";
+import { songsData } from "../src/assets/assets";
+import Player from "../components/Player";
+import React from "react";
+
+ export const Playercontext = createContext()
+
+const Playercontextprovider = (props)=>{
+
+const audioref = useRef()
+
+   const seekbg = useRef()
+   const seekbar = useRef()
+
+   const [track,settrack] = useState(songsData[0])
+   const [playerstatus,setplayerstatus] = useState(false)
+   const [time,settime] = useState({
+    currenttime:{
+        second:0,minute:0
+    },
+    totaltime:{
+        second:0,minute:0
+    }
+   })
+
+   const play = ()=>{
+    audioref.current.play();
+    setplayerstatus(true)
+   }
+
+   const pause = ()=>{
+    audioref.current.pause();
+    setplayerstatus(false)
+   }
+
+   const playwithid = async (id) =>{
+    await settrack(songsData[id]);
+    await audioref.current.play();
+    setplayerstatus(true)
+   }
+
+   const previous = async () =>{
+    if(track.id>0){
+        await settrack(songsData[track.id-1])
+        await audioref.current.play()
+        setplayerstatus(true)
+    }
+   }
+
+   const next = async () =>{
+    if(track.id<songsData.length-1){
+        await settrack(songsData[track.id+1])
+        await audioref.current.play()
+        setplayerstatus(true)
+    }
+   }
+
+   const seeksong = async (event) =>{
+        audioref.current.currentTime = ((event.nativeEvent.offsetX/seekbg.current.offsetWidth)*audioref.current.duration)
+   }
+
+   useEffect(()=>{
+      setTimeout(() => {
+        audioref.current.ontimeupdate = ()=>{
+            seekbar.current.style.width = (Math.floor(audioref.current.currentTime/audioref.current.duration*100)+"%")
+            settime({
+    currenttime:{
+        second:Math.floor(audioref.current.currentTime%60),
+        minute:Math.floor(audioref.current.currentTime/60)
+    },
+    totaltime:{
+          second:Math.floor(audioref.current.duration%60),
+        minute:Math.floor(audioref.current.duration/60)
+    }
+   })
+        }
+      }, 1000);
+   },[audioref])
+
+    const contextvalue = {
+         audioref,
+         seekbar,
+         seekbg,
+         track,settrack,
+        playerstatus,setplayerstatus,
+        time,settime,
+        play,pause,
+        playwithid,
+        previous,next,
+        seeksong
+    }
+
+    return(
+         <Playercontext.Provider value={contextvalue}>
+            {props.children}
+            
+        </Playercontext.Provider>
+    )
+
+}
+
+export default Playercontextprovider
+
---
+++ spotify-clone/src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Playercontextprovider from '../context/Playercontext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <Playercontextprovider>
+       <App />
+    </Playercontextprovider>
+    
+    </BrowserRouter>
+  </StrictMode>,
+)
